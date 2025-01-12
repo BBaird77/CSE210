8a3b8dc72bdfc037af157216aa43dd650834e92b
driab77 <144946823+driab77@users.noreply.github.com>
2025-01-11 23:01:42 -0700

    Prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,63 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade (as a decimal)? ");
+        string graderaw = Console.ReadLine();
+        double grade = double.Parse(graderaw);
+        
+        string letter;
+        
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        string sign;
+
+        double end = (grade % 10);
+
+        if (end >= 7)
+        {
+            sign = "+";
+        }
+        else if (end < 3)
+        {
+            sign = "-";
+        }
+        else
+        {
+            sign = "";
+        }
+
+        if (letter == "F" && (sign == "+" || sign == "-"))
+        {
+            sign = "";
+        }
+
+        if (letter == "A" && (sign == "+"))
+        {
+            sign = "";
+        }
+
+        string n = (letter == "A" || letter == "F") ? "n" : "";
+
+            Console.Write($"Your grade is a{n} {letter}{sign}. ");
     }
 }
