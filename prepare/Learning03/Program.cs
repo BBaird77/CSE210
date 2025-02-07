99aa795dc8664a464abf595b724cd6359a281972
driab77 <144946823+driab77@users.noreply.github.com>
2025-02-06 23:41:41 -0700

    Learning activity completed.

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction FractionOne = new Fraction();
+        Console.WriteLine(FractionOne.GetFraction());
+        Console.WriteLine(FractionOne.GetDecimal());
+
+        Fraction FractionTwo = new Fraction(5);
+        Console.WriteLine(FractionTwo.GetFraction());
+        Console.WriteLine(FractionTwo.GetDecimal());
+
+        Fraction FractionThree = new Fraction(3, 4);
+        Console.WriteLine(FractionThree.GetFraction());
+        Console.WriteLine(FractionThree.GetDecimal());
+
+        Fraction FractionFour = new Fraction(1, 3);
+        Console.WriteLine(FractionFour.GetFraction());
+        Console.WriteLine(FractionFour.GetDecimal());
     }
 }
