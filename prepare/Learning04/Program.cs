bc14539be3b405f50c84c2fc92fcf0a625f656cf
driab77 <144946823+driab77@users.noreply.github.com>
2025-02-20 22:47:31 -0700

    Learning 4

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment A = new Assignment("Samuel Bennett", "Multiplication");
+        Console.WriteLine(A.GetSummary());
+
+        MathAssignment B = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
+        Console.WriteLine(B.GetSummary());
+        Console.WriteLine(B.GetHomeworkList());
+
+        WritingAssignment C = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+        Console.WriteLine(C.GetSummary());
+        Console.WriteLine(C.GetWritingInformation());
     }
 }
