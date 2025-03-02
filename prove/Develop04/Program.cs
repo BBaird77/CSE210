5ff9f0fd66ea6720c381f07227ef876b30573cad
driab77 <144946823+driab77@users.noreply.github.com>
2025-03-01 22:14:03 -0700

    1st Phase

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -1,9 +1,26 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Develop04 World!");
+        List<Prompt> Prompt = new List<Prompt>
+        {
+            new Prompt(
+                "Think of a time when you stood up for someone else."),
+            new Prompt(
+                "Think of a time when you did something really difficult."),
+            new Prompt(
+                "Think of a time when you helped someone in need."),
+            new Prompt(
+                "Think of a time when you did something truly selfless.")
+        };
+
+        Random random = new Random();
+        Prompt Prompt = Prompt[random.Next(Prompt.Count)];
+
+        Console.WriteLine(Prompt.GetDisplayText());
     }
 }
---
+++ prove/Develop04/Prompt.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Prompt
+{
+    private Random random = new Random();
+
+    public Prompt(string text)
+    {
+       string Prompt = prompt
+    }
+
+    public string GetDisplayText()
+    {
+        return $"{Prompt}";
+    }
+
+}
+
