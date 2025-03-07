d75656eaadf1d4768c02c0a09e5dc9e642b88a96
driab77 <144946823+driab77@users.noreply.github.com>
2025-03-06 23:05:13 -0700

    Learning 05 Complete and checked.

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        Square s1 = new Square("Green", 3);
+        shapes.Add(s1);
+
+        Rectangle s2 = new Rectangle("Red", 4, 5);
+        shapes.Add(s2);
+
+        Circle s3 = new Circle("Yellow", 6);
+        shapes.Add(s3);
+
+        foreach (Shape s in shapes)
+        {
+            string color = s.GetColor();
+
+            double area = s.GetArea();
+
+            Console.WriteLine($"The {color} shape has a total area of {area} units.");
+        }
     }
 }
