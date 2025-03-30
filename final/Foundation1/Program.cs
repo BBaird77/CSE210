44096c78ac6a8c4bbcd71b02acc6feaf42bef0e4
driab77 <144946823+driab77@users.noreply.github.com>
2025-03-29 22:40:38 -0600

    Program 1 Complete

--- final/Foundation1/Program.cs
+++ final/Foundation1/Program.cs
@@ -8,7 +8,20 @@ class Program {
 
         Video video1 = new Video("Trope Talk: Silent Protagonists", "Overly Sarcastic Productions", 918);
         video1.AddComment(new Comment("Dave", "Great video, thanks!"));
+        video1.AddComment(new Comment("Bob", "Very interesting!"));
+        video1.AddComment(new Comment("Jack", "Loved the examples. Thanks for calling out my favorite!"));
         videos.Add(video1);
+
+        Video video2 = new Video("The Living Tombstone - Sunburn", "The Living Tombstone", 252);
+        video2.AddComment(new Comment("Steve", "Awesome song!"));
+        video2.AddComment(new Comment("Eva", "Amazing work!"));
+        video2.AddComment(new Comment("Danielle", "Loved the animation work!"));
+        videos.Add(video2);
+
+        Video video3 = new Video("Will Destiny 2 Frontiers Be Delayed?", "Aztecross", 965);
+        video3.AddComment(new Comment("Rex", "Man, here's hoping it doesn't."));
+        video3.AddComment(new Comment("Dan", "Looking forward to more news either way!"));
+        videos.Add(video3);
 
         foreach (var video in videos)
         {
