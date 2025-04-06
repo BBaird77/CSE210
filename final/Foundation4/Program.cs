df50c62b6330059c120321925d4bb3770d1bf19a
driab77 <144946823+driab77@users.noreply.github.com>
2025-04-05 22:09:21 -0600

    Cycling Working

--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -6,12 +6,19 @@ class Program
     static void Main(string[] args)
     {
         Running run = new Running("2025-04-05", 30, 3.0);
-
         Console.WriteLine($"Date: {run.GetDate()}");
         Console.WriteLine($"Duration: {run.GetMinutes()} minutes");
         Console.WriteLine($"Distance: {run.GetDistance()} miles");
         Console.WriteLine($"Speed: {run.GetSpeed()} mph");
         Console.WriteLine($"Pace: {run.GetPace()} min/mile");
         Console.WriteLine($"Summary: {run.GetSummary()}");
+
+        Cycling cycle = new Cycling("2025-04-05", 45, 12.0);
+        Console.WriteLine($"Date: {cycle.GetDate()}");
+        Console.WriteLine($"Duration: {cycle.GetMinutes()} minutes");
+        Console.WriteLine($"Distance: {cycle.GetDistance()} miles");
+        Console.WriteLine($"Speed: {cycle.GetSpeed()} mph");
+        Console.WriteLine($"Pace: {cycle.GetPace()} min/mile");
+        Console.WriteLine($"Summary: {cycle.GetSummary()}");
     }
 }
