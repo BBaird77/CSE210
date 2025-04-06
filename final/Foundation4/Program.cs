c53bbf062da9c6acb9f0e1659479cffd84af4a2e
driab77 <144946823+driab77@users.noreply.github.com>
2025-04-05 22:17:21 -0600

    Functioning Foundation 4 Program.

--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -6,19 +6,15 @@ class Program
     static void Main(string[] args)
     {
         Running run = new Running("2025-04-05", 30, 3.0);
-        Console.WriteLine($"Date: {run.GetDate()}");
-        Console.WriteLine($"Duration: {run.GetMinutes()} minutes");
-        Console.WriteLine($"Distance: {run.GetDistance()} miles");
-        Console.WriteLine($"Speed: {run.GetSpeed()} mph");
-        Console.WriteLine($"Pace: {run.GetPace()} min/mile");
         Console.WriteLine($"Summary: {run.GetSummary()}");
+        Console.WriteLine("");
 
         Cycling cycle = new Cycling("2025-04-05", 45, 12.0);
-        Console.WriteLine($"Date: {cycle.GetDate()}");
-        Console.WriteLine($"Duration: {cycle.GetMinutes()} minutes");
-        Console.WriteLine($"Distance: {cycle.GetDistance()} miles");
-        Console.WriteLine($"Speed: {cycle.GetSpeed()} mph");
-        Console.WriteLine($"Pace: {cycle.GetPace()} min/mile");
         Console.WriteLine($"Summary: {cycle.GetSummary()}");
+        Console.WriteLine("");
+
+        Swimming swim = new Swimming("2025-04-05", 30, 120);
+        Console.WriteLine($"Summary: {swim.GetSummary()}");
+        Console.WriteLine("");
     }
 }
--- final/Foundation4/Swimming.cs
+++ final/Foundation4/Swimming.cs
@@ -1,3 +1,15 @@
 using System;
 using System.Collections.Generic;
 
+class Swimming: Activity {
+    private int laps;
+
+    public Swimming(string date, int minutes, int laps)
+    : base(date, minutes) {
+        this.laps = laps;
+    }
+
+    public override double GetDistance() => ((laps * 50) / 1000) * 0.62;
+    public override double GetSpeed() => (GetDistance() / GetMinutes()) * 60;
+    public override double GetPace() => GetMinutes() / GetDistance();
+}
